f6bb3517600af43c4e86b43bbedaba5e2791d0af
Venky62811 <venkygokurla@gmail.com>
2025-07-21 14:50:03 +0530

    update

---
+++ venky/src/Tasks/NestedeTernaryOperator.java
@@ -0,0 +1,22 @@
+package Tasks;
+
+public class NestedeTernaryOperator {
+
+	public static void main(String[] args) {
+		boolean isLoggedin = true;
+		String 	userType =  "creator";
+		String subscriptionStatus ="expired";
+		
+		String message = !isLoggedin ? "Please log in to continue"
+                : userType.equals("admin") ? "Welcome, Admin"
+                : userType.equals("creator") && subscriptionStatus.equals("active") ? "Creator Dashboard"
+                : userType.equals("creator") && subscriptionStatus.equals("expired") ? "Renew to access your dashboard"
+                : userType.equals("viewer") && subscriptionStatus.equals("active") ? "Continue Watching"
+                : userType.equals("viewer") && subscriptionStatus.equals("expired") ? "Subscription expired"
+                : "Access Denied";
+		
+		System.out.println("Homepage : "+message);
+
+	}
+
+}
